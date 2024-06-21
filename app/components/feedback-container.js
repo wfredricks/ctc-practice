2efd10427fab595e87a99086f5488119bd6c43ed
almasbarzhaxynov <almasb@synergybis.com>
2024-06-21 14:52:48 -0400

    refactor: Add feedback functionality to FeedbackContainer component

--- app/components/feedback-container.js
+++ app/components/feedback-container.js
@@ -1,22 +1,35 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 
 export function FeedbackContainer() {
-    
-    const handleFeedback = async ({rating, message}) => {
-        const pageUrl = window.location.href;
-        const body = {
-            rating: rating,
-            message: message,
-            userid: '1234',
-            url: pageUrl
-        }
-        
-        await fetch('/api/feedback', body);
-    }
+  const [showFeedback, setShowFeedback] = React.useState(false);
 
-    handleFeedback({rating: 5, message: "Great job!"});
+  const handleFeedback = async ({ rating, message }) => {
+    const pageUrl = window.location.href;
+    const body = {
+      rating: rating,
+      message: message,
+      userid: "1234",
+      url: pageUrl,
+    };
 
-    return (<div>FeedbackContainers</div>)
+    await fetch("/api/feedback", body);
+  };
+
+  handleFeedback({ rating: 5, message: "Great job!" });
+
+  return (
+    <>
+      <button onClick={() => setShowFeedback(!showFeedback)}>
+        Add Comment
+      </button>
+      {showFeedback && (
+        <>
+          <div>commment</div>
+          <div>comments</div>
+        </>
+      )}
+    </>
+  );
 }
