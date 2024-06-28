cb1f4ea423fdfc68b6aa23c64df5331b7403661a
John Montfort <jmontfort@credence-llc.com>
2024-06-28 14:10:19 -0400

    ui

--- app/components/comment/commentPost.js
+++ app/components/comment/commentPost.js
@@ -3,9 +3,9 @@ import React from "react";
 
 const CommentPost = ({ comment }) => {
   return (
-    <div style={{padding:"1rem",borderRadius:"7px", outline:"1px solid #777", backgroundColor:"#CCC"}}>
-      <div>{comment.date}</div>
-      <div>{comment.message}</div>
+    <div style={{padding:"1rem",borderRadius:"7px", outline:"1px solid #777", backgroundColor:"#CCC", display: "flex", justifyContent: "space-between"}}>
+      <div style={{flexGrow:"1"}}>{comment.message}</div>
+      <div style={{flexGrow:"0"}}>{comment.date}</div>
     </div>
   );
 };
