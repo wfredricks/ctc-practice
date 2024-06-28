45e8daf1b19c29fd7191882cb05ec370ca68e8ff
John Montfort <jmontfort@credence-llc.com>
2024-06-28 13:52:44 -0400

    ui

--- app/components/comment/commentPost.js
+++ app/components/comment/commentPost.js
@@ -2,11 +2,6 @@
 import React from "react";
 
 const CommentPost = ({ comment }) => {
-  const handleFeedback = (e) => {
-    console.log(e.target.value);
-    setComment(e.target.value);
-  };
-
   return (
     <div style={{padding:"1rem",borderRadius:"7px", outline:"1px solid #777", backgroundColor:"#CCC"}}>
       <div>{comment.date}</div>
