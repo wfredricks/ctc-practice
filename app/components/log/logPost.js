0b0799a86752ac9561442b09fa593f80bfb0e0d3
John Montfort <jmontfort@credence-llc.com>
2024-06-28 14:28:30 -0400

    ui

--- app/components/log/logPost.js
+++ app/components/log/logPost.js
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
 
-const LogPost = ({ comment }) => {
+const LogPost = ({ log }) => {
   return (
-    <div style={{padding:"1rem",borderRadius:"7px", outline:"1px solid #777", backgroundColor:"#CCC", display: "flex", justifyContent: "space-between"}}>
-      <div style={{flexGrow:"1"}}>{comment.message}</div>
-      <div style={{flexGrow:"0"}}>{comment.date}</div>
+    <div style={{padding:"1rem",
+    // borderRadius:"7px", outline:"1px solid #777", backgroundColor:"#CCC",
+    display: "flex", justifyContent: "space-between"}}>
+      <div style={{flexGrow:"0", marginLeft:"1rem"}}>{log.user}</div>
+      <div style={{flexGrow:"1", marginLeft:"1rem"}}>{log.message}</div>
+      <div style={{flexGrow:"0", marginLeft:"1rem"}}>{log.timestamp}</div>
     </div>
   );
 };
