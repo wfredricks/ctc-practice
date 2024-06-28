bcd4f5a283125d2f886d4e0e708e69472ef88b86
John Montfort <jmontfort@credence-llc.com>
2024-06-28 14:54:20 -0400

    ui

--- app/components/comment/commentPost.js
+++ app/components/comment/commentPost.js
@@ -3,9 +3,11 @@ import React from "react";
 
 const CommentPost = ({ comment }) => {
   return (
+    <div style={{padding:".5rem"}}>
     <div style={{padding:"1rem",borderRadius:"7px", outline:"1px solid #777", backgroundColor:"#CCC", display: "flex", justifyContent: "space-between"}}>
       <div style={{flexGrow:"1"}}>{comment.message}</div>
       <div style={{flexGrow:"0"}}>{comment.date}</div>
+    </div>
     </div>
   );
 };
--- app/components/log/logPost.js
+++ app/components/log/logPost.js
@@ -3,12 +3,14 @@ import React from "react";
 
 const LogPost = ({ log }) => {
   return (
+    <div style={{padding:".5rem"}}>
     <div style={{padding:"1rem",
     // borderRadius:"7px", outline:"1px solid #777", backgroundColor:"#CCC",
     display: "flex", justifyContent: "space-between"}}>
       <div style={{flexGrow:"0", marginLeft:"1rem"}}>{log.user}</div>
       <div style={{flexGrow:"1", marginLeft:"1rem"}}>{log.message}</div>
       <div style={{flexGrow:"0", marginLeft:"1rem"}}>{log.timestamp}</div>
+    </div>
     </div>
   );
 };
